163a73b67f37db32db48fb94f24a498a0942aadc
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2018-11-01 21:47:35 -1200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const prefix = "//";
+const client = new Discord.Client();
+
+//-----------------------------------------------------------
+
+
+bot.login(NTA3NjA5MDEwMTE2MTAwMDk3.DrzMbg.WJbJnj-LO8LooJz1Cje7boOXbFE);
